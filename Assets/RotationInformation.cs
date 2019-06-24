72d8209293ca00085fc099659cc97f2d9fabdf98
Ben_Simmonds <bhsimmonds@gmail.com>
2019-06-24 14:07:26 +0100

    Everything so far

---
+++ Assets/RotationInformation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationInformation : MonoBehaviour
+{
+    public int m;
+    public float yAngle; //new angle
+    private float yAngleLast; //angle last update
+    private float deltaX; // rate of change of rotationin degrees per second 
+    public float degreesPerSecond;
+    public int headRotSpeed; // (the aim speed to be compared to)
+    public bool revFloor; // (given by the floor object, if reverberant =1)
+    public bool revRight; // (given by the right wall object, if reverberant =1)
+    public bool revLeft; // (given by the left wall object, if reverberant =1)
+    private bool sourceStatic; // (given by buttons yes = 1, no = 0)
+    float[,] responses = new float[5, 30]; // not sure about this but should be for all responses before they get written to CSV
+    List<float[]> movements = new List<float[]>(); // i can't think of another way of stoinrg what is effectively an unknonw array size. It should store deltaX against scenrario time. THis may be toom much and it way need smoothing and say 5 values a second taken instead. 
+    
+        
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+        var cameraRotation = GameObject.Find("CenterEyeAnchor").transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yAngle = transform.rotation.y;
+        deltaX += ((Mathf.Abs(yAngle - yAngleLast)) / Time.deltaTime);
+        degreesPerSecond = Time.fixedTime / deltaX;
+        Debug.Log(deltaX);
+        yAngleLast = yAngle;
+    }
+
+}
---
+++ Assets/SourceMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceMover : MonoBehaviour
+{
+    public float speed;
+    // public Vector3 cubeLocation;
+    public Rigidbody rb;
+    private float xPos;
+    private float yPos;
+    private float angle;
+    private float m; // angle multiplier;
+    public double radius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = GetComponent<Rigidbody>().position;
+        rb = GetComponent<Rigidbody>();
+        // var cameraRotation = GameObject.Find("CenterEyeAnchor").transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Find("OVRCameraRig"); // Find the game object with the script
+        RotationInformation cs = go.GetComponent<RotationInformation>(); // identify the script
+        angle = cs.yAngle;    // get the variable I want
+        m = cs.m; // get multiplier from central script
+
+        float yAngleRad = (Mathf.PI / 180) * angle * m;
+        double sinAngle = Mathf.Sin(angle);
+        double cosAngle = Mathf.Cos(angle);
+        xPos = (float) (radius * sinAngle);
+        yPos = (float) (radius * cosAngle);
+        Vector3 tempVect = new Vector3(xPos, yPos);
+        tempVect = tempVect.normalized * Time.deltaTime;
+        rb.MovePosition(transform.position + tempVect);
+    }
+}
+
